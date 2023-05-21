e344ea30f1e3e293d961cd18e5ef07d8ad02e317
lsy96999 <lsy96999@gmail.com>
2023-05-22 00:09:41 +0900

    .

---
+++ src/main/java/l/s/y/randomCardBattle/RandomCardBattleApplication.java
@@ -0,0 +1,13 @@
+package l.s.y.randomCardBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomCardBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomCardBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/l/s/y/randomCardBattle/RandomCardBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package l.s.y.randomCardBattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCardBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
