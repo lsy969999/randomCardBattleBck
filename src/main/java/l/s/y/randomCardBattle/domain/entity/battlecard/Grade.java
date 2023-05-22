c3d53a516e60d2e16de0068ae94e383af8b003e5
lsy96999 <lsy96999@gmail.com>
2023-05-22 22:15:31 +0900

    battle card entity

---
+++ src/main/java/l/s/y/randomCardBattle/domain/entity/battlecard/Grade.java
@@ -0,0 +1,16 @@
+package l.s.y.randomCardBattle.domain.entity.battlecard;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Grade {
+    COMMON("C", 0.6),
+    UNCOMMON("U", 0.3),
+    RARE("R", 0.07),
+    EPIC("E", 0.02),
+    LEGENDARY("L", 0.01);
+    private String code;
+    private Double a;
+}
---
+++ src/main/java/l/s/y/randomCardBattle/domain/entity/battlecard/Type.java
@@ -0,0 +1,5 @@
+package l.s.y.randomCardBattle.domain.entity.battlecard;
+
+public enum Type {
+    UNIT
+}
