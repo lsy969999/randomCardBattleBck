b1ce9609cbabb299cd63a42aa1889384e9bdeb9e
lsy969999 <lsy969999@gmail.com>
2023-05-25 02:36:13 +0900

    aop dependency add

---
+++ src/main/java/l/s/y/randomCardBattle/config/RcbAopConfig.java
@@ -0,0 +1,9 @@
+package l.s.y.randomCardBattle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class RcbAopConfig {
+}
